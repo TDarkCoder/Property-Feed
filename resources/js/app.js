fe1aa9bd1d41992966ec91e8fd230193095abbd4
Azizbek Khalilov <56731393+TDarkCoder@users.noreply.github.com>
2022-03-23 09:33:56 +0500

    Install and configure vue & other dependencies

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 require('./bootstrap');
+
+window.Vue = require('vue').default;
+
+import {ElementUI, locale} from './imports';
+
+Vue.use(ElementUI, {locale})
+
+component('example-component', require('./components/property/list.vue').default);
+
+const app = new Vue({
+    el: '#app',
+});
---
+++ resources/js/imports.js
@@ -0,0 +1,7 @@
+import ElementUI from 'element-ui';
+import locale from 'element-ui/lib/locale/lang/en';
+
+export {
+    ElementUI,
+    locale,
+}
